f6757ad10681b7f57d768ba14b8743feab55b14d
Padawanb <padawanb@hotmail.com>
2026-02-17 16:36:02 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
+
+const textValue = ref("Hiddy");
+const selectedValue = ref("option2");
+
+watch(textValue, (newValue, oldValue) => {
+  console.log(`Le texte est passé de ${oldValue} à ${newValue}`);
+});
+
+watch(selectedValue, (newValue, oldValue) => {
+  console.log(`L'option sélectionnée est passée de ${oldValue} à ${newValue}`);
+});
+</script>
+
+<template>
+  <div class="container">
+    <h2>Communication Parent-Enfant avec v-model</h2>
+    <p>Texte dans le parent: {{ textValue }}</p>
+    <p>Option sélectionnée dans le parent: {{ selectedValue }}</p>
+    <ChildComponent
+      v-model:textValue="textValue"
+      v-model:selectedValue="selectedValue"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+//Étape 2
+import { watch } from "vue";
+
+const props = defineProps<{ textValue: string; selectedValue: string }>();
+const emit = defineEmits<{
+  (e: "update:textValue", v: string): void;
+  (e: "update:selectedValue", v: string): void;
+}>();
+
+//Étape 3
+watch(
+  () => props.textValue,
+  (newValue) => {
+    console.log("Nouvelle valeur du champ texte :", newValue);
+  },
+);
+
+watch(
+  () => props.selectedValue,
+  (newValue) => {
+    console.log("Nouvelle valeur sélectionnée :", newValue);
+  },
+);
+</script>
+
+<template>
+  <!--Étape 4-->
+  <div>
+    <input
+      type="text"
+      :value="textValue"
+      @input="
+        emit('update:textValue', ($event.target as HTMLInputElement).value)
+      "
+    />
+
+    <select
+      :value="selectedValue"
+      @change="
+        emit('update:selectedValue', ($event.target as HTMLSelectElement).value)
+      "
+    >
+      <option value="option1">Nioh 1</option>
+      <option value="option2">Nioh 2</option>
+      <option value="option3">Nioh 3</option>
+    </select>
+  </div>
+</template>
+
+<style scoped></style>
